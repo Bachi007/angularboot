4d6f0404bf7a05efe7c0c92df4ee93095c99dc75
Bachi007 <chitturi.bhaskar007@gmail.com>
2022-11-24 11:25:57 +0530

    service integretion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BootheaderComponent } from './bootheader/bootheader.component';
+import { BootviewComponent } from './bootview/bootview.component';
+import { UserserviceService } from './service/userservice.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BootheaderComponent,
+    BootviewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
